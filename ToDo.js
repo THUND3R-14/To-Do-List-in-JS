9b5a28c516ca1947d41d635986b48583e36ee540
Naitik Pandya <72870807+THUND3R-14@users.noreply.github.com>
2023-02-09 16:13:27 +0530

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -7,201 +7,248 @@ let TaskStatus;
 
 
 //Add Task
-function setData()
-{
+function setData() {
     TaskTitle = document.getElementById("ToDoTitle").value;
     TaskDesc = document.getElementById("TodoDesc").value;
     startDate = document.getElementById("sDate").value;
     endDate = document.getElementById("eDate").value;
     TaskStatus = document.getElementById("tStatus").value;
 
-    if(validate())
-    {
+    if (validate()) {
         var myArr;
-        if(JSON.parse(window.localStorage.getItem('TData')) == null)
-        {
-            myArr = []; 
+        if (JSON.parse(window.localStorage.getItem('TData')) == null) {
+            myArr = [];
         }
-        else
-        {
+        else {
             myArr = JSON.parse(window.localStorage.getItem('TData'));
         }
-        
+
         myArr.push(
             {
-            Title : TaskTitle,
-            Desc : TaskDesc,
-            StartDate : startDate,
-            EndDate : endDate,
-            Status : TaskStatus
+                Title: TaskTitle,
+                Desc: TaskDesc,
+                StartDate: startDate,
+                EndDate: endDate,
+                Status: TaskStatus
             }
         );
-        window.localStorage.setItem('TData',JSON.stringify(myArr));
+        window.localStorage.setItem('TData', JSON.stringify(myArr));
         document.getElementById("ToDoTitle").value = '';
         document.getElementById("TodoDesc").value = '';
         document.getElementById("sDate").value = '';
         document.getElementById("eDate").value = '';
         document.getElementById("tStatus").value = '';
         window.location.href = "TaskList.html";
-     }
-    
+    }
+
 }
 
 //Show Task List
-//window.onload = ShowData();
-function ShowData()
-{
+function ShowData() {
     //window.localStorage.clear();
     var myArr;
-    if (JSON.parse(window.localStorage.getItem('TData')) === null)
-    {
+    if (JSON.parse(window.localStorage.getItem('TData')) === null) {
         myArr = [];
     }
-    else
-    {
+    else {
         myArr = JSON.parse(window.localStorage.getItem('TData'));
     }
 
     // console.log(window.localStorage.getItem('TData'));
     // document.getElementById("demo").innerHTML = myArr;
-    
-    var data = "";
-    myArr.forEach(function(element,index)
-    {
-        data += '<tr>';
-        data += '<td>' + element.Title + '</td>';
-        data += '<td>' + element.Desc + '</td>';
-        data += '<td>' + element.StartDate + '</td>';
-        data += '<td>' + element.EndDate + '</td>';
-        data += '<td>' + element.Status + '</td>';
-        data += '<td><button class="btn btn-danger" onclick = deleteTask('+ index +')>Delete</button></td>';
-        data += '<td><button class="btn btn-info" onclick = checkIndex('+ index +')>Update</button></td>';
-        data += '</tr>';
-    });
-    //console.log(myArr);
-    document.querySelector("#TaskListTable tbody").innerHTML = data;
-}
 
-//Delete Task
-function deleteTask(index)
-{
-    var taskList;
-    if (JSON.parse(window.localStorage.getItem('TData')) == null)
+    var data = "";
+    if(myArr.length == 0)
     {
-        taskList = [];
+        document.getElementById("taskTable").style.display = "none";
+        document.getElementById("message").innerHTML = 'No Tasks to do!!!';
     }
     else
     {
+        myArr.forEach(function (element, index) {
+            data += '<tr>';
+            data += '<td>' + element.Title + '</td>';
+            data += '<td>' + element.Desc + '</td>';
+            data += '<td>' + element.StartDate + '</td>';
+            data += '<td>' + element.EndDate + '</td>';
+            data += '<td>' + element.Status + '</td>';
+            data += '<td><button class="btn btn-danger" onclick = deleteTask(' + index + ')>Delete</button></td>';
+            data += '<td><button class="btn btn-info" onclick = checkIndex(' + index + ')>Edit</button></td>';
+            data += '</tr>';
+        });
+
+        document.getElementById("TaskListTable").innerHTML = data;
+    }
+   
+}
+
+//Delete Task
+function deleteTask(index) {
+    var taskList;
+    if (JSON.parse(window.localStorage.getItem('TData')) == null) {
+        taskList = [];
+    }
+    else {
         taskList = JSON.parse(window.localStorage.getItem('TData'));
     }
 
-    taskList.splice(index,1);//at position 'index' remove 1 item
-    localStorage.setItem('TData',JSON.stringify(taskList));
+    if(confirm("Are you sure do you want to delete?"))
+    {
+        taskList.splice(index, 1);//at position 'index' remove 1 item
+        localStorage.setItem('TData', JSON.stringify(taskList));
+    }
+    
+    //window.alert("Task deleted successfully!!");
     ShowData();
 }
 
 
-function checkIndex(index)
-{
-    if (index != null)
-    {
-        localStorage.setItem('TaskIndex',JSON.stringify(index));
+//Check Index
+function checkIndex(index) {
+    if (index != null) {
+        localStorage.setItem('TaskIndex', JSON.stringify(index));
         window.location.href = "ToDoList.html";
     }
-    else
-    {
+    else {
         window.location.href = "ToDoList.html";
     }
 }
 
-function getIndex()
-{
+
+//Get index
+function getIndex() {
     var index = JSON.parse(localStorage.getItem('TaskIndex'));
-    if(index != null)
-    {
-        updateTask(index);
+    if (index != null) {
+        getData(index);
     }
 }
 
-function updateTask(index)
-{
-    
+//Get Task details 
+function getData(index) {
     document.getElementById("Submit").style.display = "none";
     document.getElementById("Update").style.display = "block";
     //location.href = "ToDoList.html";
-    var myData;ToDoTitle
-    if (JSON.parse(window.localStorage.getItem('TData')) == null)
-    {
+    var myData; ToDoTitle
+    if (JSON.parse(window.localStorage.getItem('TData')) == null) {
         myData = [];
     }
-    else
-    {
+    else {
         myData = JSON.parse(window.localStorage.getItem('TData'));
     }
-    
-    
 
-   document.getElementById("ToDoTitle").value = myData[index].Title;
-   document.getElementById("TodoDesc").value = myData[index].Desc;
-   document.getElementById("sDate").value = myData[index].StartDate;
-   document.getElementById("eDate").value = myData[index].EndDate;
-   document.getElementById("tStatus").value = myData[index].Status;
+    document.getElementById("ToDoTitle").value = myData[index].Title;
+    document.getElementById("TodoDesc").value = myData[index].Desc;
+    document.getElementById("sDate").value = myData[index].StartDate;
+    document.getElementById("eDate").value = myData[index].EndDate;
+    document.getElementById("tStatus").value = myData[index].Status;
+
+    updateTask(index, myData);
+}
 
 
-   document.getElementById("Update").onclick = function()
-   {
-       if(validate())
-       {
-        myData[index].Title = document.getElementById("ToDoTitle").value;
-        myData[index].Desc = document.getElementById("TodoDesc").value ;
-        myData[index].StartDate = document.getElementById("sDate").value;
-        myData[index].EndDate = document.getElementById("eDate").value;
-        myData[index].Status = document.getElementById("tStatus").value;
-     
-        localStorage.setItem('TData',JSON.stringify(myData));
+//Update Task
+function updateTask(index, myData) {
+    document.getElementById("Update").onclick = function () {
+        TaskTitle = document.getElementById("ToDoTitle").value;
+        TaskDesc = document.getElementById("TodoDesc").value;
+        startDate = document.getElementById("sDate").value;
+        endDate = document.getElementById("eDate").value;
+        TaskStatus = document.getElementById("tStatus").value;
 
-        document.getElementById("ToDoTitle").value = '';
-        document.getElementById("TodoDesc").value = '';
-        document.getElementById("sDate").value = '';
-        document.getElementById("eDate").value = '';
-        document.getElementById("tStatus").value = '';
+        if (validate()) {
+            myData[index].Title = TaskTitle;
+            myData[index].Desc = TaskDesc;
+            myData[index].StartDate = startDate;
+            myData[index].EndDate = endDate;
+            myData[index].Status = TaskStatus;
+            // myData[index].Title = document.getElementById("ToDoTitle").value;
+            // myData[index].Desc = document.getElementById("TodoDesc").value ;
+            // myData[index].StartDate = document.getElementById("sDate").value;
+            // myData[index].EndDate = document.getElementById("eDate").value;
+            // myData[index].Status = document.getElementById("tStatus").value;
 
-        document.getElementById("Submit").style.display = "block";
-        document.getElementById("Update").style.display = "none";
+            localStorage.setItem('TData', JSON.stringify(myData));
 
-        window.localStorage.removeItem('TaskIndex');
-        window.location.href = "TaskList.html";
+            document.getElementById("ToDoTitle").value = '';
+            document.getElementById("TodoDesc").value = '';
+            document.getElementById("sDate").value = '';
+            document.getElementById("eDate").value = '';
+            document.getElementById("tStatus").value = '';
 
-       }
-   }
+            document.getElementById("Submit").style.display = "block";
+            document.getElementById("Update").style.display = "none";
+
+            window.localStorage.removeItem('TaskIndex');
+            window.alert("Task Updated!!");
+            window.location.href = "TaskList.html";
+        }
+    }
+}
+
+function showErrMsg(id,msg)
+{
+    document.getElementById(id).innerHTML = msg;
 }
 
 //Validate 
-function validate()
+function validate() 
 {
-    if(TaskTitle == "")
+    var tname = tdesc = sdate = edate = true;
+    
+
+    if (TaskTitle == "") {
+        showErrMsg("validTitle","Please enter task title!!");
+    }
+    else
     {
-        document.getElementById("validTitle").innerHTML = "Please Enter Task Title!!";
-        return false;
+        tname = false;
+    }
+
+
+    if (TaskDesc == "") {
+        showErrMsg("validDesc","Please enter task description!!");
+    }
+    else
+    {
+        tdesc = false;
+    }
+
+
+    if (startDate == "") {
+        showErrMsg("validSDate","Please select start date!!");
+    }
+    else
+    {
+        sdate = false;
     }
     
 
-    if(TaskDesc == "")
+    if (endDate == "") {
+        showErrMsg("validEDate","Please select end date!!");
+    }
+    else if(endDate < startDate)
     {
-        document.getElementById("validDesc").innerHTML = "Please Enter Task Description!!"
-        return false;
+        showErrMsg("validEDate","End date must be equal or greater than start date!!");
+    }
+    else
+    {
+        edate = false;
     }
 
-    if(startDate == "")
-    {
-        document.getElementById("validSDate").innerHTML ="Please select start date!";
-        return false;
-    }
+    // if(endDate.value < startDate.value)
+    // {
+    //     document.getElementById("validEDate").innerHTML ="End date must be equal or greater than Start date!";
+    //     return false;
+    // }
 
-    if(endDate == "")
+    if((tname || tdesc || sdate || edate) == true)
     {
-        document.getElementById("validEDate").innerHTML ="Please select end date!";
         return false;
     }
-    return true;
+    else
+    {
+        return true;
+    }
 }
+
+
